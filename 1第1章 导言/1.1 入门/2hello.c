d868e2c290838844eb26b5a31e26cbb552890255
Wu_Being <1040003585@qq.com>
2017-02-21 22:57:18 +0800

    导言 入门 第1个hello world

---
+++ 1第1章 导言/1.1 入门/2hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main()
+{
+//	printf("hello, world
+//		\n");
+	printf("hello, world\
+		\b");
+
+	printf("hello, ");
+	printf("world");
+	printf("\n");
+
+	printf("hello, world\c");
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TheCProgrammingLanguage_SecondEdition
 《C 程序设计语言》（第2版.新版）Brian W.Kernighan, Dennis M.Ritchie
+# 第1章 导言
+
